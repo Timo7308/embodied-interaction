0c97eda8107c5bc9b57e2c0aefd48f025913ea61
Amin Aghazadeh <amin_aghazadeh23@yahoo.com>
2024-07-18 21:17:07 +0200

    Links have been changed

--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <center>
-<a href="https://aminagzd.ir/embodied_interaction/" class="btn btn-light">Home Page</a>
-<a href="https://aminagzd.ir/embodied_interaction/phase_0_1.php" class="btn btn-light">New Chatbot</a>
-<a href="https://aminagzd.ir/embodied_interaction/results.php/" class="btn btn-light">Results in Tabular Format</a>
-<a href="https://aminagzd.ir/embodied_interaction/tests_data.json" class="btn btn-light" target="_blank">Results in JSON file</a>
+<a href="index.php" class="btn btn-light">Home Page</a>
+<a href="phase_0_1.php" class="btn btn-light">New Chatbot</a>
+<a href="results.php" class="btn btn-light">Results in Tabular Format</a>
+<a href="tests_data.json" class="btn btn-light" target="_blank">Results in JSON file</a>
 <hr />
 </center>
